42c396781354df55bac98993173650bb256716b4
mtalli <mauno.talli@hotmail.com>
2023-10-16 14:44:06 +0300

    html's and sql db added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"forum/internal/database"
 	"net/http"
 )
 
 func main() {
+
+	err := database.Initialize()
+	if err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to the forum!")
 	})
---
+++ internal/database/db.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *dql.DB
+
+func Initialize() error {
+	var err error
+	db, err = sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		return err
+	}
+
+	createPostsTable := `
+		CREATE TABLE IF NOT EXISTS posts (
+			id INTEGER PRIMARY KEY,
+			title TEXT NOT NULL,
+			content TEXT NOT NULL,
+			author TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);
+	`
+
+	_, err = db.Exec(createPostsTable)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetDB() *sql.DB {
+	return db
+}
