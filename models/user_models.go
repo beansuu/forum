4e19a9bd8056710a568a447f6e7cbf9715e7492c
mtalli <mauno.talli@hotmail.com>
2023-11-01 18:53:11 +0200

    user models functions and struct

--- models/user_models.go
+++ models/user_models.go
@@ -1,1 +1,11 @@
 package models
+
+// define the structure of a user
+type User struct {
+}
+
+*CreateUser(user User) error
+
+
+// retrieves a user based on their email
+func GetUserByEmail(email string)(*User,error)
