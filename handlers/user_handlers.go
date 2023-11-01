393406e0e2b0a11f7e073eef53e45927ce0a9f67
mtalli <mauno.talli@hotmail.com>
2023-11-01 18:45:19 +0200

    user handling functions

---
+++ handlers/user_handlers.go
@@ -0,0 +1,23 @@
+package handlers
+
+import "net/http"
+
+// handles user registration
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// handles user login requests
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// displays the user's dashboard
+func UserDashboardHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// handles user logout actions
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+
+}
